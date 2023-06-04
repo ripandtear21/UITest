02fc7554a9470d7662fe75edffd0d8f00cb04446
ripandtear21 <norman228337@gmail.com>
2023-06-04 22:05:16 +0200

    Update CameraFly.cs

--- Assets/Scripts/Menu/CameraFly.cs
+++ Assets/Scripts/Menu/CameraFly.cs
@@ -4,26 +4,23 @@ using UnityEngine;
 
 public class CameraFly : MonoBehaviour
 {
-    public Transform target; // Целевой объект, на который будет смотреть камера
-    public float radius = 10f; // Радиус окружности, по которой будет двигаться камера
-    public float speed = 2f; // Скорость движения камеры по окружности
+    public Transform target; 
+    public float radius = 10f; 
+    public float speed = 2f; 
 
-    private Vector3 offset; // Расстояние между камерой и целевым объектом
+    private Vector3 offset; 
 
     private void Start()
     {
-        // Вычисляем начальное смещение между камерой и целевым объектом
         offset = transform.position - target.position;
     }
 
     private void Update()
     {
-        // Вычисляем новую позицию камеры по окружности
-        float angle = Time.time * speed; // Угол поворота по окружности
+        float angle = Time.time * speed; 
         Vector3 circlePos = target.position + new Vector3(Mathf.Sin(angle), 0f, Mathf.Cos(angle)) * radius;
         transform.position = circlePos;
 
-        // Направляем камеру на целевой объект
         transform.LookAt(target);
     }
 }
